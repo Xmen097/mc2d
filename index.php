d278146376559a7450030e379a707644d22e0f8e
Petr <xsicp01@gjk.cz>
2017-07-26 22:41:33 +0200

    Upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_POST["name"]) && $_POST["name"] != "" && isset($_POST["pasw"]) && $_POST["pasw"] != "") {
 	$dbConnection = pg_connect(getenv("DATABASE_URL")) or die('Could not connect: ' . pg_last_error());
-	$query='SELECT id FROM users WHERE name=$1 AND password=$1';
+	$query='SELECT id FROM users WHERE name=$1 AND password=$2';
 	$id = pg_query($query, array($_POST["name"], /*"fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".*/$_POST["pasw"]/*."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3"*/)) or die('Query failed: ' . pg_last_error());
 	echo("id: ".$id);
 } else {
