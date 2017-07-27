7b8a9d6b175a0e3fa5474444c50dd4084aa132a2
Petr <xsicp01@gjk.cz>
2017-07-27 17:22:06 +0200

    Upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_POST["name"]) && $_POST["name"] != "" && isset($_POST["pasw"]) && $_POST["pasw"] != "") {
+if(isset($_POST["name"] && isset($_POST["pasw"])) {
 	$dbConnection = pg_connect(getenv("DATABASE_URL")) or die('Login server offline!');
 	$query="SELECT id FROM users WHERE name=$1 AND password=$2";
 	$id = pg_query_params($query, array($_POST["name"], /*"fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".*/$_POST["pasw"]/*."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3"*/));
