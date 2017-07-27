2864166ecdb55c0e1df94c42c4e3871988de68f1
Petr <xsicp01@gjk.cz>
2017-07-27 15:16:56 +0200

    Upload

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ if(isset($_POST["name"]) && $_POST["name"] != "" && isset($_POST["pasw"]) && $_P
 	$id = pg_query_params($query, array($_POST["name"], /*"fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".*/$_POST["pasw"]/*."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3"*/)) or die('Invalid name or password!');
 	$id = pg_fetch_array($id)[0];
 	if($id) {
-		$token = md5(uniqid(mt_rand(), true))
+		$token = md5(uniqid(mt_rand(), true));
 		pg_query_params("UPDATE users SET token=$1 WHERE name=$2 AND password=$3", array($token, $_POST["name"], /*"fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".*/$_POST["pasw"]/*."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3"*/)) or die('Failed to generate token!');
-		echo($token)
+		echo($token);
 	}
 } else {
 	header( 'Location: index.html' ) ;  
