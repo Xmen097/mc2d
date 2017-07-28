9607ed65afec8d591bfad69189a19c60085c0c32
Petr <xsicp01@gjk.cz>
2017-07-28 21:00:37 +0200

    Upload

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if(isset($_POST["name"]) && isset($_POST["pasw"]) && isset($_POST["type"])) {
 		$query="INSERT INTO users (name, password, id) VALUES ($1, $2, 1)";
 		pg_query_params($query, array($_POST["name"], sha1("fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".$_POST["pasw"]."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3")));
 		$token = md5(uniqid(mt_rand(), true));
-		$UPDATE =pg_query_params("UPDATE users SET token=$1 WHERE name=$2 AND password=sha1($3)", array($token, $_POST["name"], "fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".$_POST["pasw"]."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3")) or die('Failed to generate token!');
+		$UPDATE =pg_query_params("UPDATE users SET token=$1 WHERE name=$2 AND password=$3", array($token, $_POST["name"], sha1("fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".$_POST["pasw"]."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3"))) or die('Failed to generate token!');
 		echo($token);
 	}
 } else {
