00c3af403d0d59a859011aaa22410e716f192458
Petr <xsicp01@gjk.cz>
2017-07-28 21:24:46 +0200

    Upload

--- index.php
+++ index.php
@@ -13,6 +13,9 @@ if(isset($_POST["name"]) && isset($_POST["pasw"]) && isset($_POST["type"])) {
 			die('Invalid name or password!');
 		}
 	} else if($_POST["type"] == "create") {
+		if(strlen($_POST['name']) < 5 || strlen($_POST['pasw']) < 5) {
+			die("Name or password is too short (<5)");
+		}
 		$dbConnection = pg_connect(getenv("DATABASE_URL")) or die('Login server offline!');
 		$query="INSERT INTO users (name, password, id) VALUES ($1, $2, 1)";
 		pg_query_params($query, array($_POST["name"], sha1("fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".$_POST["pasw"]."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3")));
