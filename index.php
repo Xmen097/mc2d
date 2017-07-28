60f1053505ba49f50aadbd46b328c74a0295e20b
Petr <xsicp01@gjk.cz>
2017-07-28 20:36:30 +0200

    Upload

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(isset($_POST["name"]) && isset($_POST["pasw"]) && isset($_POST["type"])) {
 		}
 	} else if($_POST["type"] == "create") {
 		$dbConnection = pg_connect(getenv("DATABASE_URL")) or die('Login server offline!');
-		$query="INSERT INTO users(name, password, id) VALUES ($1, sha1($2), 1)";		if($id) {
+		$query="INSERT INTO users(name, password, id) VALUES ($1, sha1($2), 1)";
 		$token = md5(uniqid(mt_rand(), true));
 		$UPDATE =pg_query_params("UPDATE users SET token=$1 WHERE name=$2 AND password=sha1($3)", array($token, $_POST["name"], "fe453@^#/f*5%$3dw99a9/*//*2sqdw./adw2'".$_POST["pasw"]."#*fhm3/98d483d@4D4d949w4D@%#9*+#@#+56d3")) or die('Failed to generate token!');
 		echo($token);
