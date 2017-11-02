2e28bc09dd9e5934f04ea86a12f3b074451228e6
Petr <xsicp01@gjk.cz>
2017-11-02 23:18:30 +0100

    Upload

--- clickLog.js
+++ clickLog.js
@@ -46,14 +46,15 @@ onmousedown = function(event) {
 			ajax.onreadystatechange = function() {
 			if (ajax.readyState == 4) {
 				if (ajax.responseText) {
-					if(ajax.responseText != 'Login server offline' && ajax.responseText != 'Invalid name or password' && ajax.responseText != 'Failed to generate token' && ajax.responseText != 'Names can only contain alphanumeric characters') {
+					console.log(ajax.responseText != "Names can only contain alphanumeric characters");
+					if(ajax.responseText != "Login server offline" && ajax.responseText != "Invalid name or password" && ajax.responseText != "Failed to generate token" && ajax.responseText != "Names can only contain alphanumeric characters") {
 						loginToken = ajax.responseText;
 						menu.main();
 					} else {
 						alert(ajax.responseText);
 					}
 				} else {
-					alert('Login server offline!')
+					alert("Login server offline!")
 				}
 			}
 			}
