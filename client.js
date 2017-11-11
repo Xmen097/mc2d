51fe210de1c7991ab7f6a2a7d968abcf04bd1a91
Petr <xsicp01@gjk.cz>
2017-11-11 20:48:06 +0100

    Upload

---
+++ client.js
@@ -0,0 +1,140 @@
+function connectToServer() {
+	include("https://"+ip+"/socket.io/socket.io.js", function() {
+		remotePlayers=[];
+
+		function Player(gtX, gtY, gtID, gtName) {
+			this.id = gtID,
+			this.name = gtName,
+			this.x = gtX,
+			this.y = gtY;
+			this.component = new component(canvas.tileSize, 2*canvas.tileSize, "textures/player/steveRight.png", this.x, this.y, "image");
+		}
+
+		function playerById(id) {
+		    for (var i = 0; i < remotePlayers.length; i++) {
+		        if (remotePlayers[i].id == id)
+		            return remotePlayers[i];
+		    };
+		}
+
+		function onConnected() {
+		    console.log("Connected to server");
+		};
+
+		function onDisconnect(data) {
+			menus.selectMP();
+			if(data)
+				alert(data);
+		};
+
+		function onNewPlayer(data) {
+			console.log("New player has connected to server")
+			remotePlayers.push(new Player(parseInt(data.x), parseInt(data.y), parseInt(data.id), String(data.name)))
+		}
+
+		function onRemovePlayer(data) {
+			console.log("Player has disconnected")
+			var removePlayer = playerById(data.id);
+
+			if (removePlayer) {
+				remotePlayers.splice(remotePlayers.indexOf(removePlayer), 1);
+			};
+
+		}
+
+		function onNewMessage(data) {
+			if(messagesList.length==maxSavedMessages) {
+				var tempMessagesList=[];
+				for(var a=maxSavedMessages-1; a>0; a--) {
+					tempMessagesList[a-1] = messagesList[a]
+				}
+				messagesList=tempMessagesList;
+				messagesList[maxSavedMessages-1]=String(data.name)+": "+String(data.message);
+			}else
+				messagesList.push(String(data.name)+": "+String(data.message));
+		}
+
+		function onMovePlayer(data) {
+			var movePlayer=playerById(parseInt(data.id));
+			if(movePlayer) {
+				movePlayer.x=parseInt(data.x);
+				movePlayer.component.x=parseInt(data.x);
+				movePlayer.y=parseInt(data.y);
+				movePlayer.component.y=parseInt(data.y);
+				movePlayer.component.texture=parseInt(data.texture) ? "textures/player/steveRight.png" : "textures/player/steveLeft.png";
+			}
+		}
+
+		function onMapEdit(data) {
+			map[parseInt(data.x)][parseInt(data.y)] = parseInt(data.block)
+			renderMap();
+		}
+
+		function onNewMap(data) {
+			map=data;
+			renderMap();
+		}
+
+		function onInventory(data) {
+			for(var a of data) {
+				if(a.amount) {
+					if(a.id < materials.length) {
+						var item=materials[a.id];	
+					} else {
+						for(var b of items) {
+							if(b.id == a.id) {
+								item = b;
+								break;
+							}
+						}
+					}
+				} else {
+					item = undefined;
+				}
+				if(a.y < 3) {
+					inventory.inventory[a.y][a.x].item=item;
+					inventory.inventory[a.y][a.x].count=a.amount;
+				} else if(a.y == 3) {
+					inventory.hotbar[a.x].item=item;
+					inventory.hotbar[a.x].count=a.amount;
+				} else if(a.y == 4) {
+					inventory.armor[a.x].item=item;
+					inventory.armor[a.x].count=a.amount;
+				}
+			}
+
+		}
+
+		function salt(data) {
+		    socket.emit("new player", {x: player.x, y: player.y, name: name, token: sha256(loginToken+data)});
+		}
+
+		function onBlockBreaking(data) {
+			if(parseInt(data.progress) == -1) {
+				delete remoteDestroingBlock[parseInt(data.id)];
+				console.log("dwd")
+			} else {
+				remoteDestroingBlock[parseInt(data.id)] = new component(canvas.tileSize, canvas.tileSize, "textures/breaking/"+parseInt(data.progress)+".png", parseInt(data.y)*canvas.tileSize, parseInt(data.x)*canvas.tileSize, "image");
+			}
+		}
+		if(parseInt(ip.split(":")[1])) {
+			socket = io.connect(ip.split(":")[0], {port: parseInt(ip.split(":")[1]), transports: ["websocket"]});
+		}else {
+			socket = io.connect(ip.split(":")[0], {transports: ["websocket"]});
+		}
+		socket.on("connect", onConnected);
+	    socket.on("disconnect", onDisconnect);
+	    socket.on("new player", onNewPlayer);
+	    socket.on("remove player", onRemovePlayer);
+	    socket.on("move player", onMovePlayer);
+	    socket.on("map edit", onMapEdit);
+    	socket.on("new message", onNewMessage);
+    	socket.on("block breaking", onBlockBreaking);
+    	socket.on("new map", onNewMap);
+    	socket.on("salt", salt);
+    	socket.on("inventory", onInventory);
+	    if(map) {
+	    	startMP();
+	    }
+	});	
+}
---
+++ render.js
@@ -0,0 +1,83 @@
+var renderedTile = []
+
+function component(width, height, texture, x, y, itemType, percent) {
+   	this.image = new Image();
+	this.itemType = itemType;
+    this.width = width;
+    this.percent = percent||0;
+    this.height = height;
+    this.texture = texture;
+    this.x = x;
+    this.y = y; ;
+    this.update = function() {
+        if(this.itemType == "image") {
+            this.image.src = this.texture;
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }else if(this.itemType == "material") {
+      	    this.image.src = "textures/blockSheet.png";
+            context.drawImage(this.image, (this.texture.x%14)*70+1, Math.floor(this.texture.x/14)*70+1, 69, 69, this.x, this.y, this.width, this.height);
+        } else if(this.itemType == "furnaceArrow") {
+            this.image.src = "textures/ui/furnaceArrow.png";
+            context.drawImage(this.image, 0, 0, Math.round(this.percent/2.27272727273), 30, this.x, this.y, this.percent/(100/this.width), this.height);
+        } else if(this.itemType == "furnaceFire") {
+            this.image.src = "textures/ui/furnaceFire.png";
+            context.drawImage(this.image, 0, 26-Math.round(this.percent/3.84615384615), 26, Math.round(this.percent/3.84615384615), this.x, this.y+this.height-Math.round(this.percent/Math.round(100/this.height)), this.width, this.percent/(100/this.height));
+        }else if(this.itemType != undefined){
+            this.image.src = "textures/itemSheet.png";
+            context.drawImage(this.image, this.texture.x*32, this.texture.y*32, 32, 32, this.x, this.y, this.width, this.height);
+        }
+    }
+    this.gravity = function() {
+    	if(jump && timeout) {
+    		timeout=false;
+    		setTimeout(function(){jump=false;}, 150);
+    	}
+    	if(jump) {
+			return;
+    	}
+		if(map[Math.floor((this.y + this.height)/canvas.tileSize)][Math.round(this.x/canvas.tileSize)] != -1) {
+			atGround=true;
+			return;
+		}
+		if(this.x/canvas.tileSize - Math.floor(this.x/canvas.tileSize) == 0.5 && map[Math.floor((this.y + this.height)/canvas.tileSize)][Math.round(this.x/canvas.tileSize)-1] != -1) {
+			atGround=true;
+			return;
+		}
+        if(map[Math.floor((this.y + gravity + this.height)/canvas.tileSize)][Math.round(this.x/canvas.tileSize)] != -1){
+            var move= (Math.ceil((this.y + this.height)/canvas.tileSize) - (this.y + this.height)/canvas.tileSize)*canvas.tileSize
+            player.y = Math.round(move + player.y);
+            hotbarUI.y = Math.round(move + hotbarUI.y);
+            activeSlot.y = Math.round(move + activeSlot.y);
+            camera.y = Math.round(camera.y - move);
+        } else {
+            player.y +=gravity;
+            hotbarUI.y +=gravity;
+            activeSlot.y +=gravity;
+            camera.y -=gravity;
+        }
+        inventoryUI = undefined;
+        furnaceUI = undefined;
+        craftingUI=undefined;
+        for(var a of furnaceSaves) {
+            if(a.active)
+                furnaceSaves[furnaceSaves.indexOf(a)].active=false;
+        }
+        furnace = furnaceInventoryPrefab;
+        inventoryOn=false
+		renderMap();
+	}
+}
+
+function renderMap() {
+    renderedTile = [];
+	for(a=0+Math.floor(Math.abs(camera.y)/canvas.tileSize); a<(canvas.height/canvas.tileSize)+Math.ceil(Math.abs(camera.y)/canvas.tileSize);a++) {
+		for(b=0+Math.floor(camera.x/canvas.tileSize); b<(canvas.width/canvas.tileSize)+Math.ceil(camera.x/canvas.tileSize);b++) {
+			if(map[a][b] != -1)
+			{
+				if(renderedTile[a] == undefined)
+					renderedTile[a] = [];
+				renderedTile[a][b] = new component(canvas.tileSize, canvas.tileSize, materials[map[a][b]], b*canvas.tileSize, a*canvas.tileSize, "material");		
+			}
+		}
+	}
+}
