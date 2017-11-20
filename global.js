f91bcb957403f1c24ec561ecd9ae4715868701c7
Petr <xsicp01@gjk.cz>
2017-11-20 23:27:50 +0100

    Upload

--- global.js
+++ global.js
@@ -48,6 +48,22 @@ var resourceCache = {};
 var loading = [];
 var readyCallbacks = [];
 
+function include(filename, onload) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = filename;
+    script.type = 'text/javascript';
+    script.onload = script.onreadystatechange = function() {
+        if (script.readyState) {
+            if (script.readyState === 'complete' || script.readyState === 'loaded') {
+                script.onreadystatechange = null;onload();}
+            } else {
+                onload();
+            }
+        };
+    head.appendChild(script);
+}
+
 // Load an image url or an array of image urls
 function load(urlOrArr) {
     if(urlOrArr instanceof Array) {
