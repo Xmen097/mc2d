61a4fb6dad45fb244820cf0a1b467f5bf9e125b6
Petr <xsicp01@gjk.cz>
2018-01-27 19:56:54 +0100

    Upload

--- render.js
+++ render.js
@@ -53,7 +53,7 @@ function component(width, height, texture, x, y, itemType, percent) {
                 hotbarUI.y = Math.round(move + hotbarUI.y);
                 activeSlot.y = Math.round(move + activeSlot.y);
                 camera.y = Math.round(camera.y - move);
-                console.log((fallHeight-a));
+                console.log((fallHeight-a)+" , "+Math.ceil((this.y + this.height)/canvas.tileSize) - (this.y + this.height)/canvas.tileSize);
                 break;
             }
         }
