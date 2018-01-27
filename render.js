36114c319b0ac692b93c955c8d5acdc49d6a6796
Petr <xsicp01@gjk.cz>
2018-01-27 19:48:33 +0100

    Upload

--- render.js
+++ render.js
@@ -53,6 +53,7 @@ function component(width, height, texture, x, y, itemType, percent) {
                 hotbarUI.y = Math.round(move + hotbarUI.y);
                 activeSlot.y = Math.round(move + activeSlot.y);
                 camera.y = Math.round(camera.y - move);
+                console.log((fallHeight-a));
             }
         }
         if(canFall) {
