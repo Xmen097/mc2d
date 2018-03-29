d2c0c425e6ad1c1368be5d750a024a4949cb8aa1
Petr <xsicp01@gjk.cz>
2018-03-29 13:47:55 +0200

    Upload

--- breakingBlocks.js
+++ breakingBlocks.js
@@ -5,6 +5,12 @@ var breakMultiplier;
 function breakBlock(event) { 
 	var startX = Math.floor((event.pageX - document.getElementById('canvas').offsetLeft + camera.x)/canvas.tileSize);
 	var startY = Math.floor((event.pageY - document.getElementById('canvas').offsetTop + camera.y*-1)/canvas.tileSize);
+	if(activeItem.item != undefined && activeItem.item.type == items[map[startY][startX]].favType){
+		breakMultiplier=activeItem.item.multiplier;
+	}else if(activeItem.item != undefined && activeItem.item.type == items[map[startY][startX]].favType2 &&  items[map[startY][startX]].favType2 != undefined) {
+		breakMultiplier=activeItem.item.multiplier;
+	}else
+		breakMultiplier= 1;
 	if(map[startY][startX] == -1 || items[map[startY][startX]].durability == Infinity && breakMultiplier!=Infinity)
 		return;
 	if(breakMultiplier==Infinity) {
@@ -15,12 +21,6 @@ function breakBlock(event) {
 			socket.emit("map edit", {x:startY, y:startX, block: -1, active: inventory.hotbar.indexOf(activeItem)})	
 		return;
 	}
-	if(activeItem.item != undefined && activeItem.item.type == items[map[startY][startX]].favType){
-		breakMultiplier=activeItem.item.multiplier;
-	}else if(activeItem.item != undefined && activeItem.item.type == items[map[startY][startX]].favType2 &&  items[map[startY][startX]].favType2 != undefined) {
-		breakMultiplier=activeItem.item.multiplier;
-	}else
-		breakMultiplier= 1;
 	for (let i=0; i < 10;i++) {
 		destroingBlock[i] = setTimeout(function() {
 			destroingTexture = new component(canvas.tileSize, canvas.tileSize, "textures/breaking/"+i+".png", startX*canvas.tileSize, startY*canvas.tileSize, "image");
